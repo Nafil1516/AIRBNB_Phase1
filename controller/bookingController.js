4e9ec3a6f456f6734f9299ea45a650705041bb42
Nafil1516 <nafilyasir1516@hotmail.com>
2024-11-16 15:38:29 +0500

    seprate booking controller to control api

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -4,6 +4,6 @@ exports.createBooking = (req, res) => {
     if (!listingId || !checkInDate || !checkOutDate) {
       return res.status(400).json({ message: 'All booking details are required' });
     }
-    res.json({ message: 'Booking successfully created', bookingDetails: req.body });
+    res.json({ message: 'Booking creation succesfull', bookingDetails: req.body });
   };
   
